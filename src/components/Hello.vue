248c902dbe0e420d388129f279dd85617477dbd8
ZHONGY299 <2419756118@qq.com>
2020-04-14 23:52:42 +0800

    day1-hello

---
+++ src/components/Hello.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>this is a frist page!</h1>
+        <h2>{{message}}</h2>
+        <h3>{{hobbies[2]}}</h3>
+        <button @click="sayhello">sayhello</button>
+        <button @click="changeMessage('Web前端')">改变数据</button>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      message: 'hello,vue.js!',
+      hobbies: ['爬山', '跑步', '游泳']
+    }
+  },
+  methods: {
+    sayhello: function () {
+      alert('hello.vue.js!')
+    },
+    changeMessage: function (newMessage) {
+      this.message = newMessage
+      console.log(this.message)
+    }
+  }
+}
+</script>
+<style scoped>
+  h1{
+    font-size: 30px;
+    color: red;
+  }
+</style>
