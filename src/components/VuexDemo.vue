7936bc201f0009209a2668485b5898ace711de7c
ZHONGY299 <2419756118@qq.com>
2020-05-10 22:35:21 +0800

    day1-hello

--- src/components/VuexDemo.vue
+++ src/components/VuexDemo.vue
@@ -1,7 +1,39 @@
 <template>
 <div>
     {{$store.state.hello}}
-    <button @click="getVuexState">弹出Vuex的state值</button>
+    <button @click="getVuexState">获取store的state值</button>
+    <h1>{{$store.state.name}}</h1>
+    <button @click="changeName('ZHONGY')">改变Name</button>
+    <h2>{{$store.state.hello}}</h2>
+    <button @click="changeHello('this is a news!')">改变hello</button>
+    <button @click="changeTwo({'newName':'jack','newHello':'News Message!'})">改变两个</button>
+    <button @click="changeTwos">改变两个</button>
+    {{$store.state.address}}
+    <button @click="addAddress">添加</button>
+    <button @click="deleteAddress">删除</button>
+    <h3>getters</h3>
+    {{this.$store.getters.getPerson}}<br>
+    {{this.$store.getters.getAll}}<hr>
+    <h3>调用action</h3>
+    <button @click="aChangeName">调用action</button><br>
+    <button @click="ajax">调用ajax</button>
+    {{this.$store.state.users}}<br>
+    <table>
+      <tr>
+        <td>name:</td>
+        <td>password:</td>
+      </tr>
+      <!-- <tr v-for="p in this.$store.state.users" :key="p.username"> -->
+      <tr v-for="(p,index) in this.$store.state.users" :key="index">
+        <td>{{p.username}}</td>
+        <td>{{p.userpwd}}</td>
+      </tr>
+    </table>
+    <h2>多个模块</h2>
+    {{$store.state.a.privince}}
+    {{$store.state.b.privince}}
+    {{$store.state.c.privince}}
+    <button @click="changeA">改变模块A的值</button>
 </div>
 </template>
 
@@ -15,6 +47,39 @@ export default {
   methods: {
     getVuexState: function () {
       alert(this.$store.state.hello)
+    },
+    changeName: function (newName) {
+      this.$store.commit('edit', newName)
+    },
+    changeHello: function (newHello) {
+      this.$store.commit('changeHello', newHello)
+    },
+    changeTwo: function (payload) {
+      this.$store.commit('setEdit', payload)
+    },
+    changeTwos: function () {
+      this.$store.commit({
+        type: 'setEdits',
+        payload: {
+          'newName': 'TomAndJack',
+          'newHello': 'ASDFGHJKL'
+        }
+      })
+    },
+    addAddress: function () {
+      this.$store.commit('add', '陕西汉中')
+    },
+    deleteAddress: function () {
+      this.$store.commit('delete')
+    },
+    aChangeName: function () {
+      this.$store.dispatch('editAction', 'Lisa')
+    },
+    ajax: function () {
+      this.$store.dispatch('addUsersAction', [])
+    },
+    changeA: function () {
+      this.$store.commit('changePrivince', '陕西西安')
     }
   },
   // 计算属性
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,97 @@
 // 1.导入Vuex模块
 import Vue from 'vue'
 import Vuex from 'Vuex'
+import axios from 'axios'
 // 2.在Vue中使用Vuex
 Vue.use(Vuex)
+// 定义多个模块
+const moduleA = {
+  state: {
+    privince: '陕西汉中'
+  },
+  mutations: {
+    changePrivince (state, newPrivince) {
+      state.privince = newPrivince
+    }
+  }
+}
+const moduleB = {
+  state: {
+    privince: '河南郑州'
+  }
+}
+const moduleC = {
+  state: {
+    privince: '四川成都'
+  }
+}
 // 3.创建Vuex对象
 const store = new Vuex.Store({
+  modules: {
+    a: moduleA,
+    b: moduleB,
+    c: moduleC
+  },
   state: {
-    hello: 'hello,Vuex!'
+    hello: 'hello,Vuex!',
+    count: 10,
+    age: 20,
+    name: 'ZY',
+    users: []
+  },
+  mutations: {
+    // edit: function (state, newName) {
+    //   state.name = newName
+    // }
+    edit (state, newName) {
+      state.name = newName
+    },
+    changeHello: function (state, newHello) {
+      state.hello = newHello
+    },
+    setEdit: function (state, payload) {
+      state.name = payload.newName
+      state.hello = payload.newHello
+    },
+    setEdits (state, payload) {
+      state.name = payload.payload.newName
+      state.hello = payload.payload.newHello
+    },
+    add (state, address) {
+      Vue.set(state, 'address', address)
+    },
+    delete (state) {
+      Vue.delete(state, 'address')
+    },
+    addUsers (state, payload) {
+      state.users = payload
+    }
+  },
+  getters: {
+    getPerson: function (state) {
+      return state.name + ' ' + state.age
+    },
+    getAll: function (state, getters) {
+      return state.hello + ' ' + getters.getPerson
+    }
+  },
+  actions: {
+    // 3秒钟后修改Name的值
+    editAction (context, newName) {
+      setTimeout(() => {
+        context.commit('edit', newName)
+      }, 3000)
+    },
+    // payload可有可无
+    addUsersAction (context, payload) {
+      axios({
+        url: 'http://localhost:3000/api/user',
+        method: 'get'
+      }).then((resp) => {
+        console.log(resp.data)
+        context.commit('addUsers', resp.data)
+      })
+    }
   }
 })
 // 4.导出Vuex对象
